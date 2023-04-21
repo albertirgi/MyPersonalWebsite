533f4eac1354c67a3830ac53874f523f6c5d2ab1
Albert Muhammad Irgi <irgialbert@gmail.com>
2023-04-21 10:57:24 +0700

    Skills Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import About from './components/about/About';
+import Skills from './components/skills/Skills';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
     <main className='main'>
       <Home />
       <About />
+      <Skills />
     </main>
     </>
   );
