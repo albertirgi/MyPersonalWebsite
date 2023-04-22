e2af57abd6deee1803a01da24ddecfef7e39c741
Albert Muhammad Irgi <irgialbert@gmail.com>
2023-04-23 00:54:37 +0700

    Footer Component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Skills from './components/skills/Skills';
 import Services from './components/services/Services';
 import Qualification from './components/qualification/Qualification';
 import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
       {/* <Services /> */}
       <Qualification />
       <Contact />
+      <Footer />
     </main>
     </>
   );
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+        <div className="footer__container container">
+            <h1 className="footer__title">{'</> '}albertirgi{' </>'}</h1>
+
+            <ul className="footer__list"> 
+                <li>
+                    <a href="#about" className="footer__link">About</a>
+                </li>
+
+                <li>
+                    <a href="#portfolio" className="footer__link">Projects</a>
+                </li>
+
+
+            </ul>
+
+            <div className="footer__social">
+                <a href="https://www.instagram.com/albertirgi" className="footer__social-link" target="_blank">
+                    <i class="bx bxl-instagram"></i>
+                </a>
+
+                <a href="https://github.com/albertirgi" className="footer__social-link" target="_blank">
+                    <i class="bx bxl-github"></i>
+                </a>
+
+                <a href="https://www.linkedin.com/in/albertirgi" className="footer__social-link" target="_blank">
+                    <i class="bx bxl-linkedin"></i>
+                </a>
+            </div>
+
+            <span className='footer__copy'>
+                &#169; albertirgi. All rights reserved
+            </span>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
