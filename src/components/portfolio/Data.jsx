661067e8d0fcbfd35dcef2868a37a755e41032d6
Albert Muhammad Irgi <irgialbert@gmail.com>
2023-04-28 01:08:46 +0700

    Completed

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import Work1 from "../../assets/work1.jpg"
+import Work1 from "../../assets/work1.png"
+import Work2 from "../../assets/work2.png"
+import Work3 from "../../assets/work3.png"
 
 export const projectsData = [
     {
@@ -7,18 +9,21 @@ export const projectsData = [
         image: Work1,
         title: 'YOK! - Food Delivery Apps',
         category: 'design',
+        link: 'https://xd.adobe.com/view/d7ab14ef-5a54-404d-851e-fce4a89cd10e-82f8/',
     },
     {
         id: 2,
-        image: Work1,
+        image: Work2,
         title: 'BirdMate - Smart Bird Cage Apps',
         category: 'design',
+        link: 'https://www.figma.com/proto/IXjBOLKYMpRE40TyCmrNuX/SmartHome-(Community)?type=design&node-id=225-843&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A2',
     },
     {
         id: 3,
-        image: Work1,
+        image: Work3,
         title: 'My Personal Website',
         category: 'web',
+        link: 'https://github.com/albertirgi/My-Personal-Website',
     },
 ];
 
