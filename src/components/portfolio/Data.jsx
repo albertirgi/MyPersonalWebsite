b0643411e6a1372f06f447d48a2bbf8a134ae683
albertirgi <irgialbert@gmail.com>
2023-04-28 13:05:19 +0700

    Finished

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -2,28 +2,32 @@ import React from "react";
 import Work1 from "../../assets/work1.png"
 import Work2 from "../../assets/work2.png"
 import Work3 from "../../assets/work3.png"
+import Work4 from "../../assets/work4.png";
 
 export const projectsData = [
     {
         id: 1,
         image: Work1,
-        title: 'YOK! - Food Delivery Apps',
+        title: 'YOK! - Food Delivery App',
         category: 'design',
         link: 'https://xd.adobe.com/view/d7ab14ef-5a54-404d-851e-fce4a89cd10e-82f8/',
+        detail: 'View Design',
     },
     {
         id: 2,
         image: Work2,
-        title: 'BirdMate - Smart Bird Cage Apps',
+        title: 'BirdMate - Smart Bird Cage App',
         category: 'design',
         link: 'https://www.figma.com/proto/IXjBOLKYMpRE40TyCmrNuX/SmartHome-(Community)?type=design&node-id=225-843&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A2',
+        detail: 'View Design',
     },
     {
         id: 3,
-        image: Work3,
-        title: 'My Personal Website',
+        image: Work4,
+        title: '</> albertirgi </> - Personal Website',
         category: 'web',
         link: 'https://github.com/albertirgi/My-Personal-Website',
+        detail: 'Source Code',
     },
 ];
 
--- src/components/portfolio/WorkItems.jsx
+++ src/components/portfolio/WorkItems.jsx
@@ -6,7 +6,7 @@ const WorkItems = ({item}) => {
         <img src={item.image} alt="" className='work__img'/>
         <h3 className="work__title">{item.title}</h3>
         <a href={item.link} target="_blank" className="work__button">
-            View More <i className="bx bx-right-arrow-alt work__button-icon"></i>
+            {item.detail}<i className="bx bx-right-arrow-alt work__button-icon"></i>
         </a>
     </div>
   )
