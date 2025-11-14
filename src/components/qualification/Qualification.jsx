24495e7eaff34d48d8838a32ced9f9121151a94d
albertirgi <irgialbert@gmail.com>
2025-11-14 12:51:09 +0700

    update: cv

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -1,138 +1,194 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./qualification.css";
 
 const Qualification = () => {
-    const [toggleState, setToggleState] = useState(1);
+  const [toggleState, setToggleState] = useState(1);
 
-    const toggleTab = (index) => {
-        setToggleState(index);
-    };
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
   return (
-    <section className="qualification section" id='qualification'>
-        <h2 className="section__title">Qualification</h2>
-        <span className="section__subtitle">My personal journey</span>
+    <section className="qualification section" id="qualification">
+      <h2 className="section__title">Qualification</h2>
+      <span className="section__subtitle">My personal journey</span>
 
-        <div className="qualification__container container">
-            <div className="qualification__tabs">
-                <div 
-                    className={toggleState === 1 
-                    ? "qualification__button qualification__active button--flex" 
-                    : "qualification__button button--flex"}
-                    onClick={() => toggleTab(1)}>
-                    <i className="uil uil-graduation-cap qualification__icon"></i>Education
-                </div>
+      <div className="qualification__container container">
+        <div className="qualification__tabs">
+          <div
+            className={
+              toggleState === 1
+                ? "qualification__button qualification__active button--flex"
+                : "qualification__button button--flex"
+            }
+            onClick={() => toggleTab(1)}
+          >
+            <i className="uil uil-graduation-cap qualification__icon"></i>
+            Education
+          </div>
 
-                <div className={toggleState === 2 
-                    ? "qualification__button qualification__active button--flex" 
-                    : "qualification__button button--flex"}
-                    onClick={() => toggleTab(2)}>
-                    <i className="uil uil-briefcase-alt qualification__icon"></i>Experience
+          <div
+            className={
+              toggleState === 2
+                ? "qualification__button qualification__active button--flex"
+                : "qualification__button button--flex"
+            }
+            onClick={() => toggleTab(2)}
+          >
+            <i className="uil uil-briefcase-alt qualification__icon"></i>
+            Experience
+          </div>
+        </div>
+
+        <div className="qualification__sections">
+          <div
+            className={
+              toggleState === 1
+                ? "qualification__content qualification__content-active"
+                : "qualification__content"
+            }
+          >
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">
+                  Pelita Bangsa Academy
+                </h3>
+                <span className="qualification__subtitle">
+                  Blockchain Developer Bootcamp
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> Sep 2025 - Now
                 </div>
+              </div>
+            </div>
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">
+                  Petra Christian University
+                </h3>
+                <span className="qualification__subtitle">
+                  Bachelor's Degree - Computer Science
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2020 - 2024
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
             </div>
 
-            <div className="qualification__sections">
-                <div 
-                    className={toggleState === 1 
-                    ? "qualification__content qualification__content-active" 
-                    : "qualification__content"}>
-                    <div className="qualification__data">
-                        <div>
-                            <h3 className="qualification__title">Petra Christian University</h3>
-                            <span className="qualification__subtitle">Bachelor's Degree - Computer Science</span>
-                            <div className="qualification__calender">
-                                <i className="uil uil-calendar-alt"></i> 2020 - 2024
-                            </div>
-                        </div>
+            <div className="qualification__data">
+              <div></div>
 
-                        <div>
-                            <span className="qualification__rounder"></span>
-                            <span className="qualification__line"></span>
-                        </div>
-                    </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
 
-                    <div className="qualification__data">
-                        <div></div>
-
-                        <div>
-                            <span className="qualification__rounder"></span>
-                            <span className="qualification__line"></span>
-                        </div>
-
-                        <div>
-                            <h3 className="qualification__title">SMAK Santo Yoseph Denpasar</h3>
-                            <span className="qualification__subtitle">Senior High School</span>
-                            <div className="qualification__calender">
-                                <i className="uil uil-calendar-alt"></i> 2017 - 2020
-                            </div>
-                        </div>
-                    </div>
+              <div>
+                <h3 className="qualification__title">
+                  SMAK Santo Yoseph Denpasar
+                </h3>
+                <span className="qualification__subtitle">
+                  Senior High School
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2017 - 2020
                 </div>
+              </div>
+            </div>
+          </div>
 
-                <div className={toggleState === 2 
-                    ? "qualification__content qualification__content-active" 
-                    : "qualification__content"}>
-                    <div className="qualification__data">
-                        <div>
-                            <h3 className="qualification__title">Software Engineer - Freelance</h3>
-                            <span className="qualification__subtitle">ByteMe</span>
-                            <div className="qualification__calender">
-                                <i className="uil uil-calendar-alt"></i> Mar 2025 - Now
-                            </div>
-                        </div>
+          <div
+            className={
+              toggleState === 2
+                ? "qualification__content qualification__content-active"
+                : "qualification__content"
+            }
+          >
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">
+                  Software Engineer - Freelance
+                </h3>
+                <span className="qualification__subtitle">ByteMe</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> Mar 2025 - Now
+                </div>
+              </div>
 
-                        <div>
-                            <span className="qualification__rounder"></span>
-                            <span className="qualification__line"></span>
-                        </div>
-                    </div>
-                    <div className="qualification__data">
-                        <div></div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+            <div className="qualification__data">
+              <div></div>
 
-                        <div>
-                            <span className="qualification__rounder"></span>
-                            <span className="qualification__line"></span>
-                        </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
 
-                        <div>
-                            <h3 className="qualification__title">Frontend Developer - Contract</h3>
-                            <span className="qualification__subtitle">ORDO Apps</span>
-                            <div className="qualification__calender">
-                                <i className="uil uil-calendar-alt"></i> Aug 2024 - Oct 2025
-                            </div>
-                        </div>
-                    </div>
-                    <div className="qualification__data">
-                        <div>
-                            <h3 className="qualification__title">Frontend Developer - Internship</h3>
-                            <span className="qualification__subtitle">PT. Komunal Group Indonesia</span>
-                            <div className="qualification__calender">
-                                <i className="uil uil-calendar-alt"></i> June - Nov 2023
-                            </div>
-                        </div>
+              <div>
+                <h3 className="qualification__title">
+                  Frontend Developer - Contract
+                </h3>
+                <span className="qualification__subtitle">ORDO Apps</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> Aug 2024 - Oct 2025
+                </div>
+              </div>
+            </div>
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">
+                  Frontend Developer - Internship
+                </h3>
+                <span className="qualification__subtitle">
+                  PT. Komunal Group Indonesia
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> June - Nov 2023
+                </div>
+              </div>
 
-                        <div>
-                            <span className="qualification__rounder"></span>
-                            <span className="qualification__line"></span>
-                        </div>
-                    </div>
-                    <div className="qualification__data">
-                        <div></div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+            <div className="qualification__data">
+              <div></div>
 
-                        <div>
-                            <span className="qualification__rounder"></span>
-                            <span className="qualification__line"></span>
-                        </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
 
-                        <div>
-                            <h3 className="qualification__title">Kotlin Programming Fundamentals - Awardee</h3>
-                            <span className="qualification__subtitle">Baparekraf Digital Talent</span>
-                            <div className="qualification__calender">
-                                <i className="uil uil-calendar-alt"></i> May - July 2023
-                            </div>
-                        </div>
-                    </div>
+              <div>
+                <h3 className="qualification__title">
+                  Kotlin Programming Fundamentals - Awardee
+                </h3>
+                <span className="qualification__subtitle">
+                  Baparekraf Digital Talent
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> May - July 2023
+                </div>
+              </div>
+            </div>
 
-                    {/* <div className="qualification__data">
+            {/* <div className="qualification__data">
                         <div></div>
 
                         <div>
@@ -163,11 +219,11 @@ const Qualification = () => {
                             <span className="qualification__line"></span>
                         </div>
                     </div> */}
-                </div>
-            </div>
+          </div>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Qualification
+export default Qualification;
